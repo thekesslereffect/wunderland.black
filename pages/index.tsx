f40faf004d18cbae69b14ed69cb70a9a81ea5195
thekesslereffect <eldron2323@gmail.com>
2023-03-09 04:48:58 -0800

    adjustments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,79 +1,32 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2  bg-black font-overpass">
+    <div className="flex flex-col min-h-screen min-w-screen items-center justify-center py-2 bg-black font-overpass">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className="flex w-screen  flex-1 flex-col items-center justify-center px-20 text-center text-white">
+        <div className="absolute top-8 md:top-0 md:relative">
+          <h1 className="text-4xl  md:text-7xl font-bold ">_WUNDERLAND.</h1>
 
-      {/* bg-[#8247E5] */}
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center text-white">
-        <div className='absolute top-8 md:top-0 md:relative'>
-          <text className="text-7xl font-bold ">
-            {/* <text className="animate-pulse-fast">_</text>WUNDERLAND. */}
-            _WUNDERLAND.
-          </text>
-
-          <p className="mt-10 text-2xl font-bold">
+          <p className="text-lg mt-10  md:text-2xl font-bold">
             We are all mad here
           </p>
         </div>
-        
 
-        <div className='absolute bottom-8 md:bottom-48 flex w-full px-8 justify-center '>
-          <button className='btn bg-white rounded-2xl text-black font-bold text-2xl p-3 w-full md:max-w-lg '>Connect</button>
+        <div className="absolute bottom-8 md:bottom-48 flex w-full px-8 justify-center ">
+          <button className="btn bg-white rounded-2xl text-black font-bold text-2xl p-3 w-full md:max-w-lg ">
+            Connect
+          </button>
           {/* <button className='btn  rounded-2xl border-4 text-white font-bold text-2xl p-3 w-full md:max-w-lg '>> Rabbit Hole</button> */}
         </div>
-
-        {/* <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
