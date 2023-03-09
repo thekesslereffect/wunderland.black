fe50a91a1bd1362c4d7bd2a6de65fee12a3059e5
thekesslereffect <eldron2323@gmail.com>
2023-03-09 11:48:35 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,19 +10,16 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <main className="flex w-screen  flex-1 flex-col items-center justify-center px-20 text-center text-white font-overpass"> */}
       <main className="bg-black text-white font-overpass">
         <div className="flex flex-col mx-auto max-w-7xl min-h-screen py-6 sm:px-6 lg:px-8 items-center ">
           <div className="flex flex-col w-full absolute top-20 md:top-1/3 px-8 text-center ">
             <h1 className="text-4xl  md:text-7xl font-bold ">_WUNDERLAND.</h1>
-            <p className="text-md mt-10  md:text-lg font-bold">We're all mad here</p>
+            <p className="text-md mt-10  md:text-lg font-semibold">We're all mad here</p>
           </div>
 
-          <div className="flex w-full absolute bottom-8 md:bottom-1/3 px-8 justify-center">
-            <button className="btn bg-white rounded-2xl text-black font-bold text-md p-3 w-full md:max-w-lg ">
-              Connect
-            </button>
-            {/* <button className='btn  rounded-2xl border-4 text-white font-bold text-2xl p-3 w-full md:max-w-lg '>> Rabbit Hole</button> */}
+          <div className="flex w-full flex-col absolute bottom-8 md:bottom-1/3 px-8 justify-center items-center">
+            {/* <button className="btn bg-white rounded-xl text-black font-semibold text-md p-3 w-full md:max-w-lg ">Connect</button> */}
+            <button className='btn rounded-xl border-2 text-white font-semibold text-md p-3 w-full md:max-w-lg '>> Rabbit Hole</button>
           </div>
         </div>
       </main>
