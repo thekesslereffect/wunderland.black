1d5afe243e17bb9970e24014a67fc8dade69c7e4
thekesslereffect <eldron2323@gmail.com>
2023-03-09 21:57:10 -0800

    remove digital rain

--- components/index.js
+++ components/index.js
@@ -1,6 +1,5 @@
 import AddressRain from './AddressRain';
-import DigitalRain from './DigitalRain';
 import MatrixRain from './MatrixRain';
 import TextScramble from './TextScramble';
 
-export { AddressRain, DigitalRain, MatrixRain, TextScramble };
+export { AddressRain, MatrixRain, TextScramble };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { AddressRain, DigitalRain, MatrixRain, TextScramble } from '../components';
+import { AddressRain, MatrixRain, TextScramble } from '../components';
 import { useState } from "react";
 
 const Home: NextPage = () => {
   const [isHovering, setIsHovering] = useState(false);
+  const [isHoveringWVNDR, setIsHoveringWVNDR] = useState(false);
 
   return (
     // <div className="flex flex-col min-h-screen min-w-screen items-center justify-center py-2 bg-black ">
@@ -17,12 +18,13 @@ const Home: NextPage = () => {
       </Head>
       
       <main className=" bg-black text-white font-overpass">
-        {/* <AddressRain /> */}
-        <MatrixRain />
-        {/* <DigitalRain /> */}
-        <div className="flex flex-col mx-auto max-w-7xl min-h-screen py-6 sm:px-6 lg:px-8 items-center ">
+        {/* <MatrixRain /> */}
+        <div className="flex flex-col mx-auto max-w-7xl w-auto min-h-screen py-6 sm:px-6 lg:px-8 items-center ">
           <div className="flex flex-col w-full absolute top-20 md:top-1/3 px-8 text-center ">
             <h1 className="text-4xl  md:text-7xl font-bold ">_WVNDR.LAND</h1>
+            {/* <h1 className="text-4xl  md:text-7xl font-bold h-20" onMouseEnter={() => setIsHoveringWVNDR(true)} onMouseLeave={() => setIsHoveringWVNDR(false)}>
+              <TextScramble text={isHoveringWVNDR ? "WONDERLAND" : "_WVNDR.LAND"} scrambleColor={"rgba(255, 0, 130, 1)"} unscrambleColor={"white"} />
+            </h1> */}
             <p className="text-md mt-10  md:text-lg font-semibold">Not all who wander are lost</p>
           </div>
           <div className="flex w-full absolute bottom-8 md:bottom-1/3 px-8 justify-center items-center">
